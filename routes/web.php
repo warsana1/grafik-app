7a09fab94882f96252552e7b50340545c6595558
warsana <inyomanwarsana1@gmail.com>
2026-01-14 19:40:51 +0800

    Reset the code

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ChartController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/grafik', [ChartController::class, 'index']);
+
 
